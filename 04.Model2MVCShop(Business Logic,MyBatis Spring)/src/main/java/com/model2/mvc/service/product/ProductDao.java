6ebbb5bbb82064e5b539b89e8fccdbf808b89299
bit <bit@DESKTOP-KL5D1LS>
2021-05-18 17:29:07 +0900

    EE

---
+++ 04.Model2MVCShop(Business Logic,MyBatis Spring)/src/main/java/com/model2/mvc/service/product/ProductDao.java
@@ -0,0 +1,9 @@
+package com.model2.mvc.service.product;
+
+public class ProductDao {
+
+	public ProductDao() {
+		// TODO 磊悼 积己等 积己磊 胶庞
+	}
+
+}
---
+++ 04.Model2MVCShop(Business Logic,MyBatis Spring)/src/main/java/com/model2/mvc/service/product/ProductService.java
@@ -0,0 +1,9 @@
+package com.model2.mvc.service.product;
+
+public class ProductService {
+
+	public ProductService() {
+		// TODO 磊悼 积己等 积己磊 胶庞
+	}
+
+}
